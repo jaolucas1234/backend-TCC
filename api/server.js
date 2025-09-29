66c9cf577c8cc40850037b44b2ab041f5bb63e0d
jaolucas1234 <claudiarl1207@gmail.com>
2025-09-29 14:12:53 -0300

    routes

--- api/server.js
+++ api/server.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const app = express()
 const routes = require('../src/routes')
 const iaRoutesModule = require('../src/iarouter');
-const iaRoutes = iaRoutesModule.default;
+// const iaRoutes = iaRoutesModule.default;
 const port = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(routes)
-app.use('/', iaRoutes);
+// app.use('/', iaRoutes);
 
 app.listen(port, (req, res) => {
     console.log(`Servidor rodando em http://localhost:` + port);
